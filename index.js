111fa1a2de462764a219cbbbe635d792da336683
Rob <robert.gentile@powerhrg.com>
2021-05-20 19:56:52 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1= 31 ;
+const num2= 2 ;
+const multiply = num1 * num2 ;
+const random = Math.floor( Math.random() * 550 ) + 10;
+const num3 = 4; 
+const num4= 56;
+const mod = num3 % num4;
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
